284434f08bc80d5d366b47646e6d827dff0fb8b4
Matheus Jordão <matheus.jhordao@gmail.com>
2021-11-25 11:40:17 -0300

    feat: criação da classe palavra

---
+++ entidades/palavra.py
@@ -0,0 +1,6 @@
+class Palavra:
+    def __init__(self, valor, localizacao, linha, linha_localizacao):
+        self.valor = valor
+        self.localizacao = localizacao
+        self.linha = linha
+        self.linha_localizacao = linha_localizacao
