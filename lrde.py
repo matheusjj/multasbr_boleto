1a6ad3c552b63ad93aa3253d7b4c4ee03912ee30
Matheus Jordão <matheus.jhordao@gmail.com>
2021-11-25 11:33:17 -0300

    test: arquivo para a métrica do IoU

--- lrde.py
+++ lrde.py
@@ -4,14 +4,15 @@ import cv2
 import imutils
 import numpy as np
 from entidades.quadrilatero import Quadrilatero
-from detectores.detector_documento import DetectorLRDECustomizado
+from detectores.detector_documento import DetectorLRDECustomizado, DetectorDocumentoHough
 
 # altura_redim = 800
 # caminho = 'imagens/camera/'
-
+# caminho_output = 'imagens/camera/mascara_doc_hough/'
+#
 # for arquivo in os.listdir(caminho):
 #     if os.path.isfile(os.path.join(caminho, arquivo)):
-#         # print(arquivo)
+#         print(arquivo)
 #
 #         img = cv2.imread(caminho + arquivo)
 #         alt, lar = img.shape[:2]
@@ -20,19 +21,21 @@ from detectores.detector_documento import DetectorLRDECustomizado
 #         img = imutils.resize(img, height=altura_redim)
 #         ratio = alt / altura_redim
 #
-#         detector = DetectorLRDECustomizado()
-#         quad = detector(img)
+#         detector = DetectorDocumentoHough()
 #
-#         if quad is None:
+#         try:
+#             quad = detector(img)
+#         except:
 #             quad = Quadrilatero()
 #
 #         pontos_ajustados = np.array(quad.retornar_vertices() * ratio, dtype='int32')
 #         cv2.fillPoly(canvas, [pontos_ajustados], 255)
 #
-#         cv2.imwrite('imagens/camera/mascara_doc_detectado/' + arquivo, canvas)
+#         cv2.imwrite(caminho_output + arquivo, canvas)
 
-caminho_auto = 'imagens/camera/mascara_doc_detectado/'
+caminho_auto = 'imagens/camera/mascara_doc_hough/'
 caminho_manual = 'imagens/camera/mascara_documento/'
+arquivo_output = 'Resultado_IoU_Hough'
 resultados = []
 
 for arquivo in os.listdir(caminho_auto):
@@ -46,10 +49,10 @@ for arquivo in os.listdir(caminho_auto):
         iou = np.count_nonzero(intersecao == 255) / np.count_nonzero(uniao == 255)
         resultados.append([arquivo, iou])
 
-print(resultados)
+# print(resultados)
 campos = ['Imagem', 'IoU']
 
-with open('Resultado_IoU', 'w') as f:
+with open(arquivo_output, 'w') as f:
     write = csv.writer(f)
     write.writerow(campos)
     write.writerows(resultados)
