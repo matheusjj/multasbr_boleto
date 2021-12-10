3a0559d54be44df3ad5760c3f8c9bb3807283892
Matheus Jordão <matheus.jhordao@gmail.com>
2021-12-10 13:06:18 -0300

    fix: adiciona propriedade campo para a classe

--- entidades/palavra.py
+++ entidades/palavra.py
@@ -1,6 +1,7 @@
 class Palavra:
-    def __init__(self, valor, localizacao, linha, linha_localizacao):
-        self.valor = valor
+    def __init__(self, valor, localizacao, linha, linha_localizacao, bloco):
+        self.texto = valor
         self.localizacao = localizacao
         self.linha = linha
         self.linha_localizacao = linha_localizacao
+        self.bloco = bloco
