c46c5bfc394404bf182b0fce1c3695f32e616e4d
Matheus Jordão <matheus.jhordao@gmail.com>
2022-02-28 15:00:30 -0300

    chore: Move o arquivo de pré-processamento

---
+++ pre_processadores/pre_processador.py
@@ -0,0 +1,13 @@
+import cv2
+import imutils
+
+
+class PreProcessador:
+    def __init__(self, altura=800):
+        self.altura = altura
+
+    def __call__(self, imagem):
+        img = imutils.resize(imagem, height=self.altura, inter=cv2.INTER_CUBIC)
+        ratio = imagem.shape[0] / float(self.altura)
+
+        return img, ratio
